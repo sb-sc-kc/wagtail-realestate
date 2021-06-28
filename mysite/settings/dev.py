a29b6f0baf910e4c723e1f43e4fb6e6990f31990
Stéphane Peyrucq <duloup@duloup.org>
2021-06-28 21:44:04 +0200

    pipenv install wagtail; wagtail start mysite .

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-9r1y+3r(0o3ozo0#95f7gxemp42#^26d*woj=lk-vd&sz&5e(6'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
