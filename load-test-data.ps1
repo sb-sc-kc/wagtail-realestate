99586b0d866ab718f18f8a6db15aece5cf150cb2
Stephane Peyrucq <s.peyrucq@gmail.com>
2021-09-01 17:00:52 +0200

    nettoyage

--- load-test-data.ps1
+++ load-test-data.ps1
@@ -4,4 +4,3 @@ python manage.py re_pages --create
 python manage.py re_assets --create
 python manage.py re_images --create
 python manage.py re_galleries --create
-python manage.py re_offers --create
