3953f9a650f2a2d7567907f0ae473365594f8dfd
Kali Live user <kali@localhost>
2021-09-14 08:55:49 +0000

    load-test-data.sh

---
+++ load-test-data.sh
@@ -0,0 +1,6 @@
+python manage.py re_asset_types --create
+python manage.py re_users --create
+python manage.py re_pages --create
+python manage.py re_assets --create
+python manage.py re_images --create
+python manage.py re_galleries --create
